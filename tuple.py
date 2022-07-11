3a143737cee3b35b5ff27d317bb5f9c7887cd580
sunshay <sunshayina@outlook.com>
2022-07-11 16:51:53 -0700

    tuple

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# Write a Python program to sort a list of tuples using Lambda.
+# Original list of tuples: [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)] 
+# Sorting the List of Tuples:[('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
+
+
+ a = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)] 
+ b  = (sorted a,reverse = True)
+ print(b)
+ 
+# a = sorted([6,5,3,7,2,4,1])
+# print(a)
