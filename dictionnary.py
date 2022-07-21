608a923c9fd1c0d36010238fe92ae8181243d494
sunshay <sunshayina@outlook.com>
2022-07-20 23:55:28 -0700

    again

--- dictionnary.py
+++ dictionnary.py
@@ -2,3 +2,17 @@ import json
 
 a = { 'country' : 'drc' , 'ethnicity': 'black' }
 json.dump(a)
+
+
+# print( a.get('ethnicity'))
+
+# # dict = {'Name': 'Zara', 'Age': 27}
+
+# # print ("Value : %s" %  dict.get('Name'))
+
+# # name = input('who are you')
+# # print("value %s" % (name))
+
+# import json
+# d = {"a":"papa","b":"maman","c":"yaya"}
+# print(json.dump(d))
