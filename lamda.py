ff9ccb5b2c99cc04aa837351c8fb7fb5d0f03fdd
sunshay <sunshayina@outlook.com>
2022-07-21 00:01:33 -0700

    lamda

--- lamda.py
+++ lamda.py
@@ -1,11 +1,23 @@
 # write a python program that create a function that take a parameter, and that parameter will multiply with an unknown given number 
 
-from audioop import mul
-
-
 def mult(n):
     return lambda x:x * n
 
 result = mult(2)
-print f"Double the number of 15 =", result(15))
+print(result(15))
 
+# Write a Python program to create a lambda function that adds 15 to a given number passed in as an argument.
+
+def fct(a):
+    return lambda x:x+a
+
+k = fct(10)
+print(k(15))
+
+#  also create a lambda function that multiplies argument x with argument y and print the result.
+
+def res(x,y):
+    return lambda x,y:x*y
+k = res(12,4)
+print(k(12,4))
+print(k(2,3))
