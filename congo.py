a2110faf18edcf467220cb9d6b34b3e210701772
sunshay <sunshayina@outlook.com>
2022-07-21 00:01:12 -0700

    ok

--- congo.py
+++ congo.py
@@ -1,6 +1,18 @@
 import json
+from tkinter import W
 with open('congo.json') as file:
     data = json.load(file)
     
-b = json.dumps(data)
-print(b)
+# b = json.dumps(data)
+# print(b)
+
+data[0].append({
+    
+    "4":"space"
+})
+
+with open('congo.json', W) as file:
+    json.dump(data,file)
+    
+    
+    
